8272db2e9c1e2c0fae41b995269b2fa21ece050b
Mikita Kazlouski <mikita.kazlouski@gmail.com>
2020-09-11 17:54:28 +0300

    11.09.2020. Random test order fixed. Answer registration added (still not working as it should work)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const TelegramBot = require ('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api')
 require('dotenv').config()
 const TOKEN = process.env.TOKEN
 const test1Questions = [
@@ -14,31 +14,31 @@ const test1Questions = [
     'Что из перечисленного является потребностью:'
 ]
 const test1Answers = [
-    ['Самооценку','Тревогу','Страх','Гнев'],
-    ['Взаимопонимание','Самооценку','Тревогу','Радость'],
-    ['Понижаете свою самооценку','Усиливаете тревогу','Улучшаете контакт с собой','Ухудшаете свое самочувствие'],
-    ['Повышение своей самооценки','Увеличение стресса','Улучшение контакта с собой','Усиление контакта с другими'],
-    ['Повторяющийся стресс','Платье для вечера','Уменьшение стресса','Увеличение радости'],
-    ['Сближения с людьми','Улучшения взаимопонимания','Разрешения конфликтов','Избегания конфликтов'],
-    ['Правила','Потребность','Цель','Скука'],
-    ['Досада','Ненависть','Уважение','Тоска'],
-    ['Погоды','Правительства','Потребности','Мотивации'],
-    ['Безопасность','Отвращение','Презрение','Неуверенность',]
+    ['Самооценку', 'Тревогу', 'Страх', 'Гнев'],
+    ['Взаимопонимание', 'Самооценку', 'Тревогу', 'Радость'],
+    ['Понижаете свою самооценку', 'Усиливаете тревогу', 'Улучшаете контакт с собой', 'Ухудшаете свое самочувствие'],
+    ['Повышение своей самооценки', 'Увеличение стресса', 'Улучшение контакта с собой', 'Усиление контакта с другими'],
+    ['Повторяющийся стресс', 'Платье для вечера', 'Уменьшение стресса', 'Увеличение радости'],
+    ['Сближения с людьми', 'Улучшения взаимопонимания', 'Разрешения конфликтов', 'Избегания конфликтов'],
+    ['Правила', 'Потребность', 'Цель', 'Скука'],
+    ['Досада', 'Ненависть', 'Уважение', 'Тоска'],
+    ['Погоды', 'Правительства', 'Потребности', 'Мотивации'],
+    ['Безопасность', 'Отвращение', 'Презрение', 'Неуверенность',]
 ]
-const test1CorrectAnswersId = [0,2,2,1,0,3,1,2,2,0]
-let startKeyboard = ['Закрыть','Помощь']
+const test1CorrectAnswersId = [0, 2, 2, 1, 0, 3, 1, 2, 2, 0]
+let startKeyboard = ['Закрыть', 'Помощь']
 
 const bot = new TelegramBot(TOKEN, {
     polling: true
 })
-Array.prototype.insert = function ( index, item ) {
-    this.splice( index, 0, item );
+Array.prototype.insert = function (index, item) {
+    this.splice(index, 0, item);
 };
 
 //TELEGRAM BOT
 bot.onText(/\/start/, (msg) => {
-    const { id } = msg.chat
-    bot.sendMessage(id,'Привет, ' + msg.from.first_name + '!\n Какой тест ты хотел(а) бы пройти?', {
+    const {id} = msg.chat
+    bot.sendMessage(id, 'Привет, ' + msg.from.first_name + '!\n Какой тест ты хотел(а) бы пройти?', {
         reply_markup: {
             keyboard: [startKeyboard]
         }
@@ -46,7 +46,7 @@ bot.onText(/\/start/, (msg) => {
 })
 
 bot.onText(/Закрыть/, (msg) => {
-    const { id } = msg.chat
+    const {id} = msg.chat
     bot.sendMessage(id, 'Вы закрыли клавиатуру. Для продолжения работы нажмите на кнопку /start', {
         reply_markup: {
             remove_keyboard: true
@@ -55,86 +55,102 @@ bot.onText(/Закрыть/, (msg) => {
 })
 
 bot.onText(/Тест 1/, (msg) => {
-    const { id } = msg.chat
-    for (let i= 0; i<test1Questions.length; i++ ){
-        bot.sendPoll(id, test1Questions[i], test1Answers[i], {
-            type: 'quiz',
-            is_anonymous: false,
-            correct_option_id: test1CorrectAnswersId[i]
-        })
+    const {id} = msg.chat
+    let count = 0
+    let interval = setInterval(function (){
+        if (count == 10){
+            clearInterval(interval)
+        } else {
+            bot.sendPoll(id,test1Questions[count],test1Answers[count], {
+                type: 'quiz',
+                is_anonymous: false,
+                correct_option_id: test1CorrectAnswersId[count],
+            })
+            count++
+        }
+    }, 500)
+})
+
+let userAnswers = []
+
+bot.on('poll_answer', (answer) => {
+    console.log(answer)
+    if (userAnswers.length >= 10){
+        console.log('answers collected')
+
+    } else {
+        userAnswers.push(answer.option_ids[0])
+        console.log(userAnswers)
     }
 })
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 // ADDING NEW TESTS
 bot.onText(/qkrvjrpbbn/, (msg) => {
-    const { id } = msg.chat
-    startKeyboard.insert(startKeyboard.length-2, 'Тест 1')
+    const {id} = msg.chat
+    startKeyboard.insert(startKeyboard.length - 2, 'Тест 1')
     bot.sendMessage(id, 'Вы успешно добавили Тест 1 в общий пулл')
 })
 
 bot.onText(/dydwurwvoo/, (msg) => {
-    const { id } = msg.chat
-    startKeyboard.insert(startKeyboard.length-2, 'Тест 2')
+    const {id} = msg.chat
+    startKeyboard.insert(startKeyboard.length - 2, 'Тест 2')
     bot.sendMessage(id, 'Вы успешно добавили Тест 2 в общий пулл')
 })
 
 bot.onText(/arssrxbtob/, (msg) => {
-    const { id } = msg.chat
-    startKeyboard.insert(startKeyboard.length-2, 'Тест 3')
+    const {id} = msg.chat
+    startKeyboard.insert(startKeyboard.length - 2, 'Тест 3')
     bot.sendMessage(id, 'Вы успешно добавили Тест 3 в общий пулл')
 })
 
 bot.onText(/rbcqkblfbx/, (msg) => {
-    const { id } = msg.chat
-    startKeyboard.insert(startKeyboard.length-2, 'Тест 4')
+    const {id} = msg.chat
+    startKeyboard.insert(startKeyboard.length - 2, 'Тест 4')
     bot.sendMessage(id, 'Вы успешно добавили Тест 4 в общий пулл')
 })
 
 bot.onText(/xxsbnrjwil/, (msg) => {
-    const { id } = msg.chat
-    startKeyboard.insert(startKeyboard.length-2, 'Тест 5')
+    const {id} = msg.chat
+    startKeyboard.insert(startKeyboard.length - 2, 'Тест 5')
     bot.sendMessage(id, 'Вы успешно добавили Тест 5 в общий пулл')
 })
 
 bot.onText(/zxxxfxncnh/, (msg) => {
-    const { id } = msg.chat
-    startKeyboard.insert(startKeyboard.length-2, 'Тест 6')
+    const {id} = msg.chat
+    startKeyboard.insert(startKeyboard.length - 2, 'Тест 6')
     bot.sendMessage(id, 'Вы успешно добавили Тест 6 в общий пулл')
 })
 
 bot.onText(/oororodscw/, (msg) => {
-    const { id } = msg.chat
-    startKeyboard.insert(startKeyboard.length-2, 'Тест 7')
+    const {id} = msg.chat
+    startKeyboard.insert(startKeyboard.length - 2, 'Тест 7')
     bot.sendMessage(id, 'Вы успешно добавили Тест 7 в общий пулл')
 })
 
 bot.onText(/fioosudjwr/, (msg) => {
-    const { id } = msg.chat
-    startKeyboard.insert(startKeyboard.length-2, 'Тест 8')
+    const {id} = msg.chat
+    startKeyboard.insert(startKeyboard.length - 2, 'Тест 8')
     bot.sendMessage(id, 'Вы успешно добавили Тест 8 в общий пулл')
 })
 
 bot.onText(/hhemjlnoev/, (msg) => {
-    const { id } = msg.chat
-    startKeyboard.insert(startKeyboard.length-2, 'Тест 9')
+    const {id} = msg.chat
+    startKeyboard.insert(startKeyboard.length - 2, 'Тест 9')
     bot.sendMessage(id, 'Вы успешно добавили Тест 9 в общий пулл')
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // bot.onText(/помощь/, (msg) => {
